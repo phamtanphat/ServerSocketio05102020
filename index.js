6c42200979d0acfc6187d5218448b6bb63e6db15
phatdroid94 <phatdroid94@gmail.com>
2021-01-07 19:51:23 +0700

    bug

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+server.listen(process.env.PORT || 3000 ,() => console.log("Server Started"));
 
-io.on('connection', () => { /* … */ });
 
-server.listen(process.env.PORT || 3000);
+app.get('/' , function(req,res){
+    io.sockets.on('connection', function(socket){
+        console.log("Co thiet bị kết nôi");
+    });
+})
+
 
 
