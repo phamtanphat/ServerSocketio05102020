0c33617fedb8840ad347325fe39ab7eaa4edc622
phatdroid94 <phatdroid94@gmail.com>
2021-01-08 20:04:10 +0700

    sockets.on

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
+var io = require("socket.io")(server);
 server.listen(process.env.PORT || 3000 ,() => console.log("Server Started"));
 
+io.sockets.on('connection', function(socket){
+    console.log("Co thiet bị kết nôi");
+});
 
-app.get('/' , function(req,res){
-    io.sockets.on('connection', function(socket){
-        console.log("Co thiet bị kết nôi");
-    });
-})
 
 
 
